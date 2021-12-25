54cc5900fcc29adb3b144b8dd3d86d50938c72d0
Erik Galler <43548211+erikgal@users.noreply.github.com>
2021-12-25 16:46:39 +0100

    re_path

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.urls')),
+    re_path('.*',TemplateView.as_view(template_name='index.html'))
 ]
